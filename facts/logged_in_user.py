da975857ff11c1f5dee0fe9e8f6b59110a6c675d
Vaughn Miller <millerva@lafayette.edu>
2016-05-11 11:08:21 -0400

    add logged_in_user fact

---
+++ facts/logged_in_user.py
@@ -0,0 +1,15 @@
+'''Get the current logged in user'''
+
+import subprocess
+from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+import sys
+
+def fact():
+    '''Get the current logged in user'''
+    try:
+    	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+    	username = [username,""][username in [u"loginwindow", None, u""]]
+    except (IOError, OSError):
+        pass
+
+    return {'logged_in_user': username}
